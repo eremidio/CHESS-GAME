a66a0b869feb2a1d35da6262fc2c82971a10263b
ELIAS RODRIGUES EMÍDIO <120426530+eremidio@users.noreply.github.com>
2023-05-03 10:16:20 -0300

    Update xadrez_metodos_mainloop.hpp

--- xadrez_metodos_mainloop.hpp
+++ xadrez_metodos_mainloop.hpp
@@ -18,27 +18,41 @@ print_board();
 //****************************************************************************************************************************
 //FUNÇÃO QUE EXECUTA O LOOP PRINCIPAL DO JOGO
 void chess_game::mainloop(){
+
 //LOOP PRINCIPAL
 while(true){
+
 //BRANCAS REALIZAM O LANCE PRIMEIRO
 white_to_move();
 print_board();
+
 //EM CASO DE EMPATE OU CHEQUE O JOGO É ENCERRADO
 //CHEQUEMATE
 if(checkmate==true)
 break;
+
 //EMPATE
 //PROPONDO EMPATE POR REPETIÇÃO
-cout<<"::: (Pressione enter para continuar ou digite 'e' para propor empate) ";
+cout<<"::: (Pressione enter para continuar, digite 'e' para propor empate ou digite 'a' para para abandonar o jogo) ";
 int flag1=cin.get();
 char wd = getchar();
+
 if(wd=='e')
 common_agreement_draw();
+if(wd=='a')
+resignate();
+
+//ABANDONANDO O JOGO
+if(resignation==true)
+break;
+
+
 //CHECANDO OUTROS CRITÉRIOS DE EMPATE
 fifth_rule_draw();
 repetition_draw();
 insufficient_material_draw();
 black_stalemate_draw();
+
 if(draw_game==true){
 draw_warning();
 break;
@@ -52,13 +66,21 @@ print_board();
 //CHEQUEMATE
 if(checkmate==true)
 break;
+
 //EMPATE
 //PROPONDO EMPATE POR REPETIÇÃO
-cout<<"::: (Pressione enter para continuar ou digite 'e' para propor empate) ";
+cout<<"::: (Pressione enter para continuar, digite 'e' para propor empate ou digite 'a' para para abandonar o jogo) ";
 int flag2=cin.get();
 char bd = getchar();
 if(bd=='e')
 common_agreement_draw();
+if(bd=='a')
+resignate();
+//ABANDONANDO O JOGO
+if(resignation==true)
+break;
+
+
 //CHECANDO OUTROS CRITÉRIOS DE EMPATE
 fifth_rule_draw();
 repetition_draw();
@@ -72,5 +94,5 @@ break;
 
 
 
-           };
+           };//FIM DO LOOP PRINCIPAL
                            };
